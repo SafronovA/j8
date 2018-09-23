504dd7cf9b1f60ae736de17c4539b1997c6589d5
Alexey Safronov <alexey.safronov96@gmail.com>
2018-09-23 21:45:02 +0300

    f

--- IdeaProjects/java8tr/src/main/java/code/impl2interf/J.java
+++
@@ -1,7 +0,0 @@
-package code.impl2interf;
-
-public interface J {
-    default void show(){
-        System.out.println(" J ");
-    }
-}
--- IdeaProjects/java8tr/src/main/java/code/impl2interf/K.java
+++
@@ -1,7 +0,0 @@
-package code.impl2interf;
-
-public interface K {
-    default void show(){
-        System.out.println(" K ");
-    }
-}
